fc011a9402fbfb53bd57b060a8ae718febdec030
LucienXuan <1106232004@scholar.mila.edu.my>
2026-04-14 08:18:32 +0000

    Lab 2

--- server.js
+++ server.js
@@ -2,28 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (request, response)=>{
-    response.send('<h1>Welcome to MILA University!</h1><p>This is something.</p>');
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get('/about', (request, response)=>{
-    response.send('<h1>MILA University is a university in Malaysia.</h1><p>MILA University is located in Nilai, Negeri Sembilan.</p>');
+app.post('/submit', (req, res) => {
+  const submittedName = req.body.studentName;
+  const submittedEmail = req.body.studentEmail;
+  const submittedID = req.body.studentID;
+  const submittedProgram = req.body.program;
+
+  res.render('success', {
+    name: submittedName,
+    email: submittedEmail,
+    studentID: submittedID,
+    program: submittedProgram
+  });
 });
 
-app.get('/contact', (request, response)=>{
-    response.send('<h1>Contact Page</h1><p>Please contact Mr. Adam for any inquiries at adam@milaedu.</p>');
-});
-
-app.get('/school', (request, response) => {
-  response.send(`
-    <h1>School of Engineering and Computing</h1><br>
-    <p><strong>Dean:</strong> Prof. Adam</p>
-    <p><strong>Head of Department for Computing:</strong> Prof. Bella</p>
-    <p><strong>Head of Department for Chemical Engineering:</strong> Prof. Charlie</p>
-    <p><strong>Head of Department for Mechanical Engineering:</strong> Prof. Darvy</p>
-  `);
-});
-
-app.listen(PORT, ()=> {
-    console.log(`Server is successfully running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is successfully running on port ${PORT}`);
 });
