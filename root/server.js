deae2df514bdd94d3d242c76d6778677c2b9cfd6
LucienXuan <1106232004@scholar.mila.edu.my>
2026-05-05 04:43:03 +0000

    Lab 5

--- root/server.js
+++ root/server.js
@@ -13,7 +13,7 @@ app.use(session({
 }));
 
 app.set('view engine', 'ejs'); 
-app.set('views', '../views');
+app.set('views', './views');
 app.use(express.urlencoded({ extended: true })); 
 
 // RBAC Middleware - Role-Based Access Control
